7d75046ad46f1271d74b04233437cf6d115ff430
thekiwidev <adedotungabriel20@gmail.com>
2022-11-23 00:26:51 +0100

    the landing page is done without the beautification, next up, the blobs and patterns

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-document.querySelector(".menu-icon").addEventListener("click", () => {
-  document.querySelector(".menu-icon").classList.toggle("open");
+document.querySelector(".menu").addEventListener("click", () => {
+  document.querySelector(".menu").classList.toggle("open");
 });
